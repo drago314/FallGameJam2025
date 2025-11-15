2ae63dfc09444bb3e408dc004fcc3e6828d87fba
ixavon1 <ixavsigler@gmail.com>
2025-11-14 22:10:12 -0500

    zombie

--- Assets/Scripts/MainGame/Player.cs
+++ Assets/Scripts/MainGame/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,8 +11,10 @@ public class Player : MonoBehaviour
     public Transform body;
     float defaultBodyScale;
 
-    public float moveSpeed;
+    public float moveSpeed, sprintMod;
     Vector2 direction;
+
+    public PlayerWeapon pw;
 
     private void Start()
     {
@@ -21,8 +24,6 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        float speedMod = 1;
-
         int x = 0;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) x = -1;
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) x = 1;
@@ -31,15 +32,16 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) y = 1;
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) y = -1;
 
-        direction = new Vector2(x, y).normalized;
+        // DIRECTION controls player direction AND move speed AND anim speed
+        direction = new Vector2(x, y).normalized * moveSpeed * (Input.GetKey(KeyCode.LeftShift) ? sprintMod : 1);
 
         // animations
         if (x != 0) body.localScale = new Vector3(x > 0 ? defaultBodyScale : -defaultBodyScale, body.localScale.y, 1);
-        anim.SetFloat("Speed", direction.magnitude);
+        anim.SetFloat("Speed", direction.magnitude / moveSpeed);
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = direction * moveSpeed;
+        rb.velocity = direction;
     }
 }
