ca653201c6ebf26bd90bfb429936c534bd5e192e
ixavon1 <ixavsigler@gmail.com>
2025-11-15 20:03:40 -0500

    menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Canvas canvas;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    void Update()
+    {
+        Vector2 mousePos;
+
+        // Convert mouse screen position → UI position
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            Input.mousePosition,
+            canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera,
+            out mousePos
+        );
+
+        rectTransform.anchoredPosition = mousePos;
+    }
+
+    public void GoToGame() { SceneManager.LoadScene("MainGame"); }
+}
